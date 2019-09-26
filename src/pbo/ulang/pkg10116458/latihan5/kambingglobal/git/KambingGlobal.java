d3426336b88c797c029d670be78cc160d4768df8
nanda <nanda@nanda>
2019-09-26 21:06:15 +0700

    commit men

---
+++ src/pbo/ulang/pkg10116458/latihan5/kambingglobal/git/KambingGlobal.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10116458.latihan5.kambingglobal.git;
+
+/**
+ *
+ * @author nanda
+ */
+public class KambingGlobal {
+    
+    int jumlahKambing = 88;
+    
+    //Method untuk menampilkan jumlah kambing
+    public void getJumlahKambing() {
+        System.out.println("Jumlah kambing " + jumlahKambing);
+    }
+    
+    public void tambahKambing() {
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing setelah di tambah:  " + jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        KambingGlobal kambingSusu = new KambingGlobal();
+        
+        // Menampilkan jumlah kambing yang ada saat program pertama x berjalan
+        kambingSusu.getJumlahKambing();
+        
+        // Menambah satu kambing
+        kambingSusu.tambahKambing();
+        
+        // menampilkan jumlah kambing yang ada
+        kambingSusu.getJumlahKambing();
+        
+        
+    }
+    
+}
